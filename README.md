80ed56e2610937cb0315b89e64de5f5bf9ee4b2d
BS <bs@BSs-Mac-Pro.local>
2018-09-02 13:49:14 +0200

    organize file and add hot reload module

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## Some front end starters kit.
+# 1 - First install Parcel globaly 
+yarn global add parcel-bundler
+or
+npm install -g parcel-bundler
+
+# 2 - Create the package.json in the root of your folder
+yarn init -y
---
+++ _Parcel/src/js/index.js
@@ -0,0 +1,11 @@
+console.log("hello worlddddd");
+
+(function(){
+  const _select = element => document.querySelector(element)
+  setInterval( ( ) => {
+    _select('body').style.background = 'pink'
+  }, 5000)
+  setTimeout( ( ) => {
+    location.reload();
+  }, 10000)
+}())
